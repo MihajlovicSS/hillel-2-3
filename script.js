b4b404597196006ec69708536d18bbe1faa8f4f4
MihajlovicSS <mihajlovicsinisa1456780@gmail.com>
2023-02-07 07:06:44 +0200

    Second version

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function calculation(firstNum, secondNum, operand) {
     }
 }
 
-function add(a, b){return a + b}
+function add(a, b){return Number(a) + Number(b)}
 function subtract(a, b){return a - b}
 function multiply(a, b){return a * b}
 function divide(a, b){return a / b}
